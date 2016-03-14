fb5ea7cafb3168d3e525a37ac130b8c9da15b282
Jasper den Ouden <o.jasper@gmail.com>
2016-03-14 16:32:30 +0100

    Okey, some memoization.

--- alt_require/alt_require.lua
+++ alt_require/alt_require.lua
@@ -1,11 +1,15 @@
+local already_found = {}
+
 -- Wonder why lua doesnt expose something like this already.
 local function alt_findfile(str)
+   if already_found[str] then return already_found[str] end
    local pos = string.gsub(str, "[.]", "/")
    for at in string.gmatch(package.path, "[^;]+") do
       local cur_file = string.gsub(at, "[?]", pos)
       local open = io.open(cur_file)
       if open then
          io.close(open) 
+         already_found[str] = cur_file
          return cur_file
       end
    end
