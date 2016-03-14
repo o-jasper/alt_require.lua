d8435f9d635efabc94dd154ceaff6bd6b90a34f3
Jasper den Ouden <o.jasper@gmail.com>
2016-03-14 16:30:12 +0100

    got rid of `already_loaded`, which wasnt being used, got rid of `string_split` by using `string.gmatch`

--- alt_require/alt_require.lua
+++ alt_require/alt_require.lua
@@ -1,24 +1,7 @@
-local Public = {}
-
-local function string_split(str, by)
-   local by = by or " "
-   local pi, i, j, ret = 1, nil, nil, {}
-   while true do
-      i, j = string.find(str, by, pi)
-      if not i then
-         table.insert(ret, string.sub(str, pi))
-         return ret
-      else
-         table.insert(ret, string.sub(str, pi, i - 1))
-         pi = j + 1
-      end
-   end
-end
-
--- Wonder why lua doesnt expose something like this.
-function Public.alt_findfile(str)
-   for _,at in pairs(string_split(package.path, ";")) do
-      local pos = string.gsub(str, "[.]", "/")
+-- Wonder why lua doesnt expose something like this already.
+local function alt_findfile(str)
+   local pos = string.gsub(str, "[.]", "/")
+   for at in string.gmatch(package.path, "[^;]+") do
       local cur_file = string.gsub(at, "[?]", pos)
       local open = io.open(cur_file)
       if open then
@@ -27,20 +10,12 @@ function Public.alt_findfile(str)
       end
    end
 end
-
-Public.already_loaded = {}
-
-function Public.alt_require_plain(meta)
+-- Overly lazy.
+local function alt_require_plain(meta)
    return function(str)
-      local got = Public.already_loaded[str]
-      if got then
-         return got
-      else
-         local file = Public.alt_findfile(str)
-         -- TODO for `file == nil`, what should the behavior be?
-         return file and loadfile(file, nil, setmetatable({}, meta))()
-      end   
+      return loadfile(assert(alt_findfile(str)),
+                      nil, setmetatable({}, meta))()
    end
 end
 
-return Public
+return { alt_findfile=alt_findfile, alt_require_plain=alt_require_plain }
