e8e7c3b3fc1b2ff2dcdedeced7fc539ff4a668ba
Jasper den Ouden <o.jasper@gmail.com>
2016-03-15 17:16:21 +0100

    Some testing based on other programs/tests that can be combined with this. (test doesnt pass)

---
+++ alt_require/test/client_on/top_in_client.lua
@@ -0,0 +1,17 @@
+local alt_require = require("alt_require").require
+
+local sender = require("alt_require.client.http"):new{
+   under_site  = "http://localhost:26019/",
+   under_path = "alt_require",
+}
+
+-- Only the given file is run locally, rest on server side.
+local function req(str)
+   return alt_require({in_package=str}, {},
+      sender:globals(nil, req),
+      require "alt_require.glob.simple"
+      --{ __envname="reqself", require = req }
+   )(str)()
+end
+
+req(arg[1])
