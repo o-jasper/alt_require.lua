bd9366c01f40f0d6076a08309383c76c94f0ac41
Jasper den Ouden <o.jasper@gmail.com>
2016-03-28 02:32:49 +0200

    Make printing of test/server.lua shorter.

--- alt_require/test/server.lua
+++ alt_require/test/server.lua
@@ -3,11 +3,23 @@ local globals = { require = require }
 local receiver = require("alt_require.server.pegasus"):new{
    globals=globals, under_path="alt_require"}
 
+local prevstr, prevstr_n = 0
 require("pegasus"):new{ port=26019}:start(function(req, rep)
       local method, name, id = receiver:pegasus_respond(req, rep)
       if not method then
          rep:addHeader('Content-Type', 'bin/storebin'):write("No response")
       else
-         print(method, name, id)
+         local str = string.format("%s\t%s\t%s", method, name, id)
+         if str == prevstr then
+            prevstr_n = prevstr_n + 1
+         else
+            prevstr = str
+            if prevstr_n == 0 then
+               print(str)
+            else
+               print(str, string.format("x%s", prevstr_n))
+               prevstr_n = 0
+            end
+         end
       end
 end)
